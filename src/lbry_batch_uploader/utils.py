6ced3a249893ff9cdb19ea58ba246102def23a0f
thk-cheng <kenneth.cheng.tsun.him@gmail.com>
2022-05-06 00:52:32 +0100

    Add more unit test for the utils module
    Tested `_pipe_cmds`.
    Also, temporarily commented out `ThumbnailCreationError` and `create_thumbnail` for further inspection.

--- src/lbry_batch_uploader/utils.py
+++ src/lbry_batch_uploader/utils.py
@@ -11,30 +11,30 @@ class Error(Exception):
 class PipeError(Error):
     """Exception raised for error in piping commands."""
     pass
-
-class ThumbnailCreationError(Error):
-    """
-    Exception raised for error in creating thumbnail from an input.
-
-    Attributes
-    ----------
-    file_name_absolute: str
-        The absolute path of the input file
-
-    p: subprocess.CompletedProcess
-        An instance of CompletedProcess for the command
-            that attempts to create the thumbnail
-
-    """
-
-    def __init__(self, file_name_absolute: str, p: CompletedProcess):
-        self.file_name_absolute = file_name_absolute
-        self.p = p
-        super().__init__()
-
-    def __str__(self):
-        return f'Fail to create thumbnail for {self.file_name_absolute}'
-
+ 
+# class ThumbnailCreationError(Error):
+#     """
+#     Exception raised for error in creating thumbnail from an input.
+# 
+#     Attributes
+#     ----------
+#     file_name_absolute: str
+#         The absolute path of the input file
+# 
+#     p: subprocess.CompletedProcess
+#         An instance of CompletedProcess for the command
+#             that attempts to create the thumbnail
+# 
+#     """
+# 
+#     def __init__(self, file_name_absolute: str, p: CompletedProcess):
+#         self.file_name_absolute = file_name_absolute
+#         self.p = p
+#         super().__init__()
+# 
+#     def __str__(self):
+#         return f'Fail to create thumbnail for {self.file_name_absolute}'
+ 
 
 def get_file_name_no_ext(file_name_with_ext: str) -> str:
     """
@@ -61,79 +61,79 @@ def get_file_name_no_ext(file_name_with_ext: str) -> str:
     return '.'.join(name_parts)
 
 
-def create_thumbnail(base_path: str, file_name: str) -> str:
-    """
-    Create a thumbnail from the input file.
-
-    The selected frame is chosen to be at the middle of the input file.
-
-    Parameters
-    ----------
-    base_path: str
-        The base path which contains the input file
-
-    file_name: str
-        The name of input file with extension
-
-    Returns
-    -------
-    str
-        The name of the thumbnail with extension
-
-    """
-
-    file_name_absolute = os.path.join(base_path, file_name)
-    thumbnail_name = f'{get_file_name_no_ext(file_name)}.png'
-    thumbnail_name_absolute = os.path.join(base_path, thumbnail_name)
-
-    # Explanation for all the commands below:
-    #   cmd_0: Get info of the input file by ffmpeg
-    #   cmd_1: Grep the line that contains the duration of input file
-    #   cmd_2: Isolate the duration of video from that line
-    #   cmd_3: Remove trailing comma
-    #   cmd_4: Find the time corresponds to the middle of the input file
-    #   cmd_5: Actual command that creates the thumbnail by ffmpeg
-
-    cmd_0 = ['ffmpeg', '-i', file_name_absolute]
-    cmd_1 = ['grep', 'Duration']
-    cmd_2 = ['awk', '{print $2}']
-    cmd_3 = ['tr', '-d', ',']
-    cmd_4 = ['awk', '-F', ':', '{print ($3+$2*60+$1*3600)/2}']
-
-    # Same as adding "2>&1" at the end of cmd_0
-    p = subprocess.run(cmd_0, stdout=PIPE, stderr=STDOUT, text=True)
-
-    # Pipe commands from cmd_0 to cmd_4
-    p = _pipe_cmds([cmd_1, cmd_2, cmd_3, cmd_4], prev_p=p)
-
-    # # Format the delayed start time as "00:00:00.000"
-    # delayed_time_in_sec = float(p.stdout)
-    # delayed_hour = delayed_time_in_sec // 3600
-    # delayed_min = (delayed_time_in_sec - delayed_hour * 3600) // 60
-    # delayed_sec, delayed_millisec = divmod(delayed_time_in_sec - delayed_hour * 3600 - delayed_min * 60, 1)
-    # delayed_start_time = f'{int(delayed_hour):02}:{int(delayed_min):02}:{int(delayed_sec):02}.{delayed_millisec*1000:.0f}'
-
-    # Get the delayed start time from p
-    delayed_start_time = str(float(p.stdout))
-
-    cmd_5 = [
-        'ffmpeg',
-        '-y',
-        '-i', file_name_absolute,
-        '-frames:v', '1',
-        '-ss', delayed_start_time,
-        thumbnail_name_absolute
-    ]
-
-    p1 = subprocess.run(cmd_5, capture_output=True, text=True)
-
-    if p1.returncode != 0:
-        raise ThumbnailCreationError(file_name_absolute, p1)
-    else:
-        print(f'Successfully created thumbnail for {file_name_absolute}')
-
-    return thumbnail_name
-
+# def create_thumbnail(base_path: str, file_name: str) -> str:
+#     """
+#     Create a thumbnail from the input file.
+# 
+#     The selected frame is chosen to be at the middle of the input file.
+# 
+#     Parameters
+#     ----------
+#     base_path: str
+#         The base path which contains the input file
+# 
+#     file_name: str
+#         The name of input file with extension
+# 
+#     Returns
+#     -------
+#     str
+#         The name of the thumbnail with extension
+# 
+#     """
+# 
+#     file_name_absolute = os.path.join(base_path, file_name)
+#     thumbnail_name = f'{get_file_name_no_ext(file_name)}.png'
+#     thumbnail_name_absolute = os.path.join(base_path, thumbnail_name)
+# 
+#     # Explanation for all the commands below:
+#     #   cmd_0: Get info of the input file by ffmpeg
+#     #   cmd_1: Grep the line that contains the duration of input file
+#     #   cmd_2: Isolate the duration of video from that line
+#     #   cmd_3: Remove trailing comma
+#     #   cmd_4: Find the time corresponds to the middle of the input file
+#     #   cmd_5: Actual command that creates the thumbnail by ffmpeg
+# 
+#     cmd_0 = ['ffmpeg', '-i', file_name_absolute]
+#     cmd_1 = ['grep', 'Duration']
+#     cmd_2 = ['awk', '{print $2}']
+#     cmd_3 = ['tr', '-d', ',']
+#     cmd_4 = ['awk', '-F', ':', '{print ($3+$2*60+$1*3600)/2}']
+# 
+#     # Same as adding "2>&1" at the end of cmd_0
+#     p = subprocess.run(cmd_0, stdout=PIPE, stderr=STDOUT, text=True)
+# 
+#     # Pipe commands from cmd_0 to cmd_4
+#     p = _pipe_cmds([cmd_1, cmd_2, cmd_3, cmd_4], prev_p=p)
+# 
+#     # # Format the delayed start time as "00:00:00.000"
+#     # delayed_time_in_sec = float(p.stdout)
+#     # delayed_hour = delayed_time_in_sec // 3600
+#     # delayed_min = (delayed_time_in_sec - delayed_hour * 3600) // 60
+#     # delayed_sec, delayed_millisec = divmod(delayed_time_in_sec - delayed_hour * 3600 - delayed_min * 60, 1)
+#     # delayed_start_time = f'{int(delayed_hour):02}:{int(delayed_min):02}:{int(delayed_sec):02}.{delayed_millisec*1000:.0f}'
+# 
+#     # Get the delayed start time from p
+#     delayed_start_time = str(float(p.stdout))
+# 
+#     cmd_5 = [
+#         'ffmpeg',
+#         '-y',
+#         '-i', file_name_absolute,
+#         '-frames:v', '1',
+#         '-ss', delayed_start_time,
+#         thumbnail_name_absolute
+#     ]
+# 
+#     p1 = subprocess.run(cmd_5, capture_output=True, text=True)
+# 
+#     if p1.returncode != 0:
+#         raise ThumbnailCreationError(file_name_absolute, p1)
+#     else:
+#         print(f'Successfully created thumbnail for {file_name_absolute}')
+# 
+#     return thumbnail_name
+ 
 
 def _pipe_cmds(cmds: list[list[str]], *, prev_p: CompletedProcess = None) -> CompletedProcess:
     """
@@ -172,11 +172,12 @@ def _pipe_cmds(cmds: list[list[str]], *, prev_p: CompletedProcess = None) -> Com
     return p
 
 
-def main():
-    base_path = '/Users/kennethcheng/Downloads/lbry-upload/short-videos'
-    file_name = 'Shortest Video on Youtube Part 1 [tPEE9ZwTmy0].webm'
-    print(create_thumbnail(base_path, file_name))
+# def main():
+#     base_path = '/Users/kennethcheng/Downloads/lbry-upload/short-videos'
+#     file_name = 'Shortest Video on Youtube Part 1 [tPEE9ZwTmy0].webm'
+#     print(create_thumbnail(base_path, file_name))
 
 
 if __name__ == '__main__':
-    main()
+    pass
+
